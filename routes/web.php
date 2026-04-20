14150daeecf68ea13d39f1316bf34f187231fbd4
Neath <neath@gmail.com>
2026-04-20 11:47:40 +0700

    add rentcontroller with web.php and use arisable

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,16 @@ use App\Http\Controllers\LeaseController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\MaintenanceRequestController;
 use App\Http\Controllers\TenantController;
+use App\Http\Controllers\RentController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/welcome', function () {
     return view('welcome');
 });
 
+Route::get('/rent', [RentController::class,'index']);
 
-Route::get('/app', function () {
-    return view('app');
-});
+
 
 
  
