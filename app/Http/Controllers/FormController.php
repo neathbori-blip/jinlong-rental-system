ae77259dc717fae3acdd904075ef9c0bae7113b2
Neath <neathBori@gmail.com>
2026-04-22 00:04:37 +0700

    post data witn varisable to database

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -1,19 +1,11 @@
 <?php
 
-
- namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
 
 class FormController extends Controller
 {
-    public function create() {
-        return view('form');
-    }
 
-    public function store(Request $request) {
-    dd($request);
-    }
 }
-
